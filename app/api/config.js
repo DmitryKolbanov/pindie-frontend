48f1fbe4373a878a93d3cbe98edd3df7db3043e1
DmitryKolbanov <kolbanov.dmitry@yandex.ru>
2024-05-24 15:54:36 +0400

    url

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "callban-backend.nomoredomainswork.ru/api";
+export const BASE_URL = "https://callban-pindie.nomoredomainswork.ru//api";
 // export const BASE_URL = "http://localhost:3001/api";
 
 export const endpoints = {
